6e6ec40f26b1269ad0d9d11eba343d74230e55f0
SolPulido <144694649+SolPulido@users.noreply.github.com>
2024-03-19 13:04:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+ # Task Make
+# Write a program that outputs a joke on multiple lines. 
+
+#sol pulido
+#09/18/23
+
+print("What falls,but never needs a bandage? \n\n The rain.\n")
+print("What did one toilet say to another? \n\n You look flushed")
+
